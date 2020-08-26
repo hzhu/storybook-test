71bbb6e2ea1b450420037c91ec96691f5058aff4
Jinwoo Leo Jeong <thinkratomos@gmail.com>
2020-08-26 21:02:53 +0900

    Test decorator

---
+++ .storybook/preview.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
+
+// export const decorators = [(Story) => (
+//   <div>
+//     hello!
+//     <Story />
+//   </div>
+// )]
